a0c355ff0e3cc7469de253217011371271998043
Kemal Kurniawan <kemal@kkurniawan.com>
2019-01-26 05:42:37 +0700

    Make coverage 100%

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -18,7 +18,7 @@ def test_init_samples_non_sequence():
 
 @pytest.fixture
 def dataset():
-    return Dataset(range(5))
+    return Dataset(list(range(5)))
 
 
 def test_getitem(dataset):
@@ -30,30 +30,43 @@ def test_len(dataset):
     assert len(dataset) == 5
 
 
-def test_shuffle(setup_rng, dataset):
-    before = list(dataset)
-    retval = dataset.shuffle()
-    assert retval is dataset
-    assert len(dataset) == len(before)
-    assert all(s in dataset for s in before)
-    assert list(dataset) != before
+class TestShuffle:
+    @pytest.fixture
+    def tuple_dataset(self):
+        return Dataset(tuple(range(5)))
+
+    def assert_shuffle(self, dataset):
+        before = list(dataset)
+        retval = dataset.shuffle()
+        after = list(dataset)
+
+        assert retval is dataset
+        assert len(before) == len(after)
+        assert all(v in after for v in before)
+        assert before != after
+
+    def test_mutable_seq(self, setup_rng, dataset):
+        self.assert_shuffle(dataset)
+
+    def test_immutable_seq(self, setup_rng, tuple_dataset):
+        self.assert_shuffle(tuple_dataset)
 
 
 def test_batch(dataset):
     minibatches = dataset.batch(2)
     assert isinstance(minibatches, Sequence)
     assert len(minibatches) == 3
-    assert minibatches[0] == range(0, 2)
-    assert minibatches[1] == range(2, 4)
-    assert minibatches[2] == range(4, 5)
+    assert minibatches[0] == [0, 1]
+    assert minibatches[1] == [2, 3]
+    assert minibatches[2] == [4]
 
 
 def test_batch_exactly(dataset):
     minibatches = dataset.batch_exactly(2)
     assert isinstance(minibatches, Sequence)
     assert len(minibatches) == 2
-    assert minibatches[0] == range(0, 2)
-    assert minibatches[1] == range(2, 4)
+    assert minibatches[0] == [0, 1]
+    assert minibatches[1] == [2, 3]
 
 
 def test_batch_nonpositive_batch_size(dataset):
