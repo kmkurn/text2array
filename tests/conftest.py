2bdd1b1eefba8da4ecb5155cb47fce95f9a1a9d0
Kemal Kurniawan <kemal@kkurniawan.com>
2019-01-27 15:32:47 +0700

    Handle when there are more than one str fields

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,7 +11,7 @@ def setup_rng():
 
 @pytest.fixture
 def samples():
-    return [TestSample(i, (i + 1) / 3, f'word-{i}') for i in range(5)]
+    return [TestSample(i, (i + 1) / 3, f'word-{i}', f'token-{i}') for i in range(5)]
 
 
 @pytest.fixture
@@ -30,21 +30,24 @@ def stream_dataset(stream):
 
 
 class TestSample(SampleABC):
-    def __init__(self, x, y, z):
+    def __init__(self, x, y, z, w):
         self.x = x
         self.y = y
         self.z = z
+        self.w = w
 
     def __lt__(self, s):
         if self.x != s.x:
             return self.x < s.x
         if self.y != s.y:
             return self.y < s.y
-        return self.z < s.z
+        if self.z != s.z:
+            return self.z < s.z
+        return self.w < s.w
 
     @property
     def fields(self):
-        return {'x': self.x, 'y': self.y, 'z': self.z}
+        return {'x': self.x, 'y': self.y, 'z': self.z, 'w': self.w}
 
 
 class Stream:
