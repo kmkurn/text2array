ca2ebeb32dfbf789bced93babd35b1f98ba226ab
Kemal Kurniawan <kemal@kkurniawan.com>
2019-01-27 20:42:41 +0700

    Add a todo

--- text2array/samples.py
+++ text2array/samples.py
@@ -3,3 +3,5 @@ from typing import Mapping, Sequence, Union
 FieldName = str
 FieldValue = Union[float, int, Sequence[float], Sequence[int]]
 Sample = Mapping[FieldName, FieldValue]
+
+# TODO handle when field value is a seq of seq of float/int
