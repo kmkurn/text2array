f50422f1860b0e8b1de82cc4b6d9c861e64d25db
Kemal Kurniawan <kemal@kkurniawan.com>
2019-01-27 13:25:04 +0700

    Remove Batches stuff and refactor

--- text2array/__init__.py
+++ text2array/__init__.py
@@ -1,12 +1,10 @@
+from .batches import Batch
 from .datasets import Dataset, StreamDataset
-from .batches import Batch, Batches, StreamBatches
 from .samples import Sample
 
 __all__ = [
     Sample,
+    Batch,
     Dataset,
     StreamDataset,
-    Batch,
-    Batches,
-    StreamBatches,
 ]
