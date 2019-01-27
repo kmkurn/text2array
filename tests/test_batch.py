752f9b8ac2572a0e89f5d5815fef834304f8fed9
Kemal Kurniawan <kemal@kkurniawan.com>
2019-01-28 06:59:27 +0700

    Refactor tests for Batch.to_array() into a class

--- tests/test_batch.py
+++ tests/test_batch.py
@@ -48,48 +48,50 @@ def test_get_invalid_name(batch):
     assert "some samples have no field 'foo'" in str(exc.value)
 
 
-def test_to_array(batch):
-    arr = batch.to_array()
-    assert isinstance(arr, Mapping)
+class TestToArray:
+    def test_ok(self, batch):
+        arr = batch.to_array()
+        assert isinstance(arr, Mapping)
 
-    assert isinstance(arr['i'], np.ndarray)
-    assert arr['i'].shape == (len(batch), )
-    assert arr['i'].tolist() == list(batch.get('i'))
+        assert isinstance(arr['i'], np.ndarray)
+        assert arr['i'].shape == (len(batch), )
+        assert arr['i'].tolist() == list(batch.get('i'))
 
-    assert isinstance(arr['f'], np.ndarray)
-    assert arr['f'].shape == (len(batch), )
-    for i in range(len(batch)):
-        assert arr['f'][i] == pytest.approx(batch[i]['f'])
+        assert isinstance(arr['f'], np.ndarray)
+        assert arr['f'].shape == (len(batch), )
+        for i in range(len(batch)):
+            assert arr['f'][i] == pytest.approx(batch[i]['f'])
 
-    assert isinstance(arr['is'], np.ndarray)
-    maxlen = max(len(x) for x in batch.get('is'))
-    assert arr['is'].shape == (len(batch), maxlen)
-    for r, s in zip(arr['is'], batch):
-        assert r[:len(s['is'])].tolist() == list(s['is'])
-        assert all(c == 0 for c in r[len(s['is']):])
+    def test_seq(self, batch):
+        arr = batch.to_array()
 
-    assert isinstance(arr['fs'], np.ndarray)
-    maxlen = max(len(x) for x in batch.get('fs'))
-    assert arr['fs'].shape == (len(batch), maxlen)
-    for r, s in zip(arr['fs'], batch):
-        for c, f in zip(r, s['fs']):
-            assert c == pytest.approx(f)
-        assert all(c == pytest.approx(0, abs=1e-7) for c in r[len(s['fs']):])
+        assert isinstance(arr['is'], np.ndarray)
+        maxlen = max(len(x) for x in batch.get('is'))
+        assert arr['is'].shape == (len(batch), maxlen)
+        for r, s in zip(arr['is'], batch):
+            assert r[:len(s['is'])].tolist() == list(s['is'])
+            assert all(c == 0 for c in r[len(s['is']):])
 
+        assert isinstance(arr['fs'], np.ndarray)
+        maxlen = max(len(x) for x in batch.get('fs'))
+        assert arr['fs'].shape == (len(batch), maxlen)
+        for r, s in zip(arr['fs'], batch):
+            for c, f in zip(r, s['fs']):
+                assert c == pytest.approx(f)
+            assert all(c == pytest.approx(0, abs=1e-7) for c in r[len(s['fs']):])
 
-def test_to_array_custom_padding(batch):
-    arr = batch.to_array(pad_with=1)
-    for r, s in zip(arr['is'], batch):
-        assert all(c == 1 for c in r[len(s['is']):])
-    for r, s in zip(arr['fs'], batch):
-        assert all(c == pytest.approx(1) for c in r[len(s['fs']):])
+    def test_custom_padding(self, batch):
+        arr = batch.to_array(pad_with=1)
+        for r, s in zip(arr['is'], batch):
+            assert all(c == 1 for c in r[len(s['is']):])
+        for r, s in zip(arr['fs'], batch):
+            assert all(c == pytest.approx(1) for c in r[len(s['fs']):])
 
+    def test_no_common_field_names(self, samples):
+        samples_ = list(samples)
+        samples_.append({'foo': 10})
+        batch = Batch(samples_)
 
-def test_to_array_no_common_field_names(samples):
-    samples_ = list(samples)
-    samples_.append({'foo': 10})
-    batch = Batch(samples_)
-
-    with pytest.raises(RuntimeError) as exc:
-        batch.to_array()
-    assert 'some samples have no common field names with the others' in str(exc.value)
+        with pytest.raises(RuntimeError) as exc:
+            batch.to_array()
+        assert 'some samples have no common field names with the others' in str(exc.value)
