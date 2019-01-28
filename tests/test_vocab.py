b67cecedd06c8f7db2fa72cfacf0f51f1b743a68
Kemal Kurniawan <kemal@kkurniawan.com>
2019-01-29 05:37:52 +0700

    Change the Vocab API; now it's a mapping to str-to-int mapping

--- tests/test_vocab.py
+++ tests/test_vocab.py
@@ -1,11 +1,10 @@
-from collections.abc import Mapping, Sequence
+from collections.abc import Mapping
 
 import pytest
 
 from text2array import Vocab
 
 
-# TODO change so vocab['w'] returns the mapping, not sequence
 class TestFromSamples():
     def test_ok(self):
         ss = [{'w': 'c'}, {'w': 'b'}, {'w': 'a'}, {'w': 'b'}, {'w': 'c'}, {'w': 'c'}]
@@ -16,29 +15,24 @@ class TestFromSamples():
         assert list(vocab) == ['w']
 
         itos = '<pad> <unk> c b'.split()
-        assert isinstance(vocab['w'], Sequence)
+        assert isinstance(vocab['w'], Mapping)
         assert len(vocab['w']) == len(itos)
-        for i in range(len(itos)):
-            assert vocab['w'][i] == itos[i]
+        assert list(vocab['w']) == itos
+        for i, w in enumerate(itos):
+            assert vocab['w'][w] == i
 
-        assert isinstance(vocab['w'].stoi, Mapping)
-        assert len(vocab['w'].stoi) == len(vocab['w'])
-        assert set(vocab['w'].stoi) == set(vocab['w'])
-        for i, s in enumerate(vocab['w']):
-            assert vocab['w'].stoi[s] == i
-
-        assert vocab['w'].stoi['foo'] == vocab['w'].stoi['<unk>']
-        assert vocab['w'].stoi['bar'] == vocab['w'].stoi['<unk>']
+        assert vocab['w']['foo'] == vocab['w']['<unk>']
+        assert vocab['w']['bar'] == vocab['w']['<unk>']
 
     def test_has_vocab_for_all_str_fields(self):
         ss = [{'w': 'b', 't': 'b'}, {'w': 'b', 't': 'b'}]
         vocab = Vocab.from_samples(ss)
-        assert isinstance(vocab['t'], Sequence)
-        assert isinstance(vocab['t'].stoi, Mapping)
+        assert 't' in vocab
 
     def test_no_vocab_for_non_str(self):
         vocab = Vocab.from_samples([{'i': 10}, {'i': 20}])
-        with pytest.raises(RuntimeError) as exc:
+        assert 'i' not in vocab
+        with pytest.raises(KeyError) as exc:
             vocab['i']
         assert "no vocabulary found for field name 'i'" in str(exc.value)
 
@@ -47,17 +41,11 @@ class TestFromSamples():
         vocab = Vocab.from_samples(ss)
 
         itos = '<pad> <unk> c b'.split()
-
-        assert isinstance(vocab['ws'], Sequence)
+        assert isinstance(vocab['ws'], Mapping)
         assert len(vocab['ws']) == len(itos)
-        for i in range(len(itos)):
-            assert vocab['ws'][i] == itos[i]
-
-        assert isinstance(vocab['ws'].stoi, Mapping)
-        assert len(vocab['ws'].stoi) == len(vocab['ws'])
-        assert set(vocab['ws'].stoi) == set(vocab['ws'])
-        for i, s in enumerate(vocab['ws']):
-            assert vocab['ws'].stoi[s] == i
+        assert list(vocab['ws']) == itos
+        for i, w in enumerate(itos):
+            assert vocab['ws'][w] == i
 
     def test_seq_of_seq(self):
         ss = [{
@@ -70,17 +58,11 @@ class TestFromSamples():
         vocab = Vocab.from_samples(ss)
 
         itos = '<pad> <unk> d c b'.split()
-
-        assert isinstance(vocab['cs'], Sequence)
+        assert isinstance(vocab['cs'], Mapping)
         assert len(vocab['cs']) == len(itos)
-        for i in range(len(itos)):
-            assert vocab['cs'][i] == itos[i]
-
-        assert isinstance(vocab['cs'].stoi, Mapping)
-        assert len(vocab['cs'].stoi) == len(vocab['cs'])
-        assert set(vocab['cs'].stoi) == set(vocab['cs'])
-        for i, s in enumerate(vocab['cs']):
-            assert vocab['cs'].stoi[s] == i
+        assert list(vocab['cs']) == itos
+        for i, w in enumerate(itos):
+            assert vocab['cs'][w] == i
 
     def test_empty_samples(self):
         vocab = Vocab.from_samples([])
@@ -111,6 +93,6 @@ class TestFromSamples():
             'w': 'c',
             't': 'c'
         }]
-        vocab = Vocab.from_samples(ss, ve_kwargs={'w': dict(min_count=3)})
+        vocab = Vocab.from_samples(ss, options={'w': dict(min_count=3)})
         assert list(vocab['w']) == ['<pad>', '<unk>', 'c']
         assert list(vocab['t']) == ['<pad>', '<unk>', 'c', 'b']
