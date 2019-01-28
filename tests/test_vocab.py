13d753471ac414b0aaa58b381219c64c6ab4592e
Kemal Kurniawan <kemal@kkurniawan.com>
2019-01-28 18:13:57 +0700

    Make Vocab a mapping

--- tests/test_vocab.py
+++ tests/test_vocab.py
@@ -9,30 +9,32 @@ class TestFromDataset():
     def test_ok(self):
         dat = Dataset([{'w': 'c'}, {'w': 'b'}, {'w': 'a'}, {'w': 'b'}, {'w': 'c'}, {'w': 'c'}])
         vocab = Vocab.from_dataset(dat)
+        assert isinstance(vocab, Mapping)
+
         itos = '<pad> <unk> c b'.split()
 
-        assert isinstance(vocab.of('w'), Sequence)
-        assert len(vocab.of('w')) == len(itos)
+        assert isinstance(vocab['w'], Sequence)
+        assert len(vocab['w']) == len(itos)
         for i in range(len(itos)):
-            assert vocab.of('w')[i] == itos[i]
+            assert vocab['w'][i] == itos[i]
 
-        assert isinstance(vocab.of('w').stoi, Mapping)
-        assert len(vocab.of('w').stoi) == len(vocab.of('w'))
-        assert set(vocab.of('w').stoi) == set(vocab.of('w'))
-        for i, s in enumerate(vocab.of('w')):
-            assert vocab.of('w').stoi[s] == i
+        assert isinstance(vocab['w'].stoi, Mapping)
+        assert len(vocab['w'].stoi) == len(vocab['w'])
+        assert set(vocab['w'].stoi) == set(vocab['w'])
+        for i, s in enumerate(vocab['w']):
+            assert vocab['w'].stoi[s] == i
 
-        assert vocab.of('w').stoi['foo'] == vocab.of('w').stoi['<unk>']
-        assert vocab.of('w').stoi['bar'] == vocab.of('w').stoi['<unk>']
+        assert vocab['w'].stoi['foo'] == vocab['w'].stoi['<unk>']
+        assert vocab['w'].stoi['bar'] == vocab['w'].stoi['<unk>']
 
     def test_has_vocab_for_all_str_fields(self):
         dat = Dataset([{'w': 'b', 't': 'b'}, {'w': 'b', 't': 'b'}])
         vocab = Vocab.from_dataset(dat)
-        assert isinstance(vocab.of('t'), Sequence)
-        assert isinstance(vocab.of('t').stoi, Mapping)
+        assert isinstance(vocab['t'], Sequence)
+        assert isinstance(vocab['t'].stoi, Mapping)
 
     def test_no_vocab_for_non_str(self):
         vocab = Vocab.from_dataset(Dataset([{'i': 10}, {'i': 20}]))
         with pytest.raises(RuntimeError) as exc:
-            vocab.of('i')
+            vocab['i']
         assert "no vocabulary found for field name 'i'" in str(exc.value)
