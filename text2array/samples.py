d62cfb2d1d7d2a92b5e6c4a8257f8bf46d41345a
Kemal Kurniawan <kemal@kkurniawan.com>
2019-01-28 10:13:07 +0700

    Support recursive sequences for field values

--- text2array/samples.py
+++ text2array/samples.py
@@ -1,7 +1,7 @@
 from typing import Mapping, Sequence, Union
 
+# TODO remove these "type ignore" once mypy supports recursive types
+# see: https://github.com/python/mypy/issues/731
 FieldName = str
-FieldValue = Union[float, int, Sequence[float], Sequence[int]]
-Sample = Mapping[FieldName, FieldValue]
-
-# TODO handle when field value is a seq of seq of float/int
+FieldValue = Union[float, int, Sequence['FieldValue']]  # type: ignore
+Sample = Mapping[FieldName, FieldValue]  # type: ignore
