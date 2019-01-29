11ff9b84a8d95191a8a393352f2144a73c555f73
Kemal Kurniawan <kemal@kkurniawan.com>
2019-01-29 21:53:15 +0700

    Organize tests for Dataset.apply_vocab() in a class

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -121,81 +121,80 @@ def test_batch_nonpositive_batch_size(dataset):
     assert 'batch size must be greater than 0' in str(exc.value)
 
 
-def test_apply_vocab():
-    dat = Dataset([{
-        'w': 'a',
-        'ws': ['a', 'b'],
-        'cs': [['a', 'b'], ['b', 'a']],
-        'i': 10,
-        'j': 20
-    }, {
-        'w': 'b',
-        'ws': ['a', 'a'],
-        'cs': [['b', 'b'], ['b', 'a']],
-        'i': 10,
-        'j': 20
-    }])
-    vocab = {
-        'w': {
-            'a': 0,
-            'b': 1
-        },
-        'ws': {
-            'a': 2,
-            'b': 3
-        },
-        'cs': {
-            'a': 4,
-            'b': 5
-        },
-        'j': {
-            20: 2
+class TestApplyVocab:
+    def test_ok(self):
+        dat = Dataset([{
+            'w': 'a',
+            'ws': ['a', 'b'],
+            'cs': [['a', 'b'], ['b', 'a']],
+            'i': 10,
+            'j': 20
+        }, {
+            'w': 'b',
+            'ws': ['a', 'a'],
+            'cs': [['b', 'b'], ['b', 'a']],
+            'i': 10,
+            'j': 20
+        }])
+        vocab = {
+            'w': {
+                'a': 0,
+                'b': 1
+            },
+            'ws': {
+                'a': 2,
+                'b': 3
+            },
+            'cs': {
+                'a': 4,
+                'b': 5
+            },
+            'j': {
+                20: 2
+            }
         }
-    }
-    dat = dat.apply_vocab(vocab)
-    assert list(dat) == [{
-        'w': 0,
-        'ws': [2, 3],
-        'cs': [[4, 5], [5, 4]],
-        'i': 10,
-        'j': 2
-    }, {
-        'w': 1,
-        'ws': [2, 2],
-        'cs': [[5, 5], [5, 4]],
-        'i': 10,
-        'j': 2
-    }]
+        dat = dat.apply_vocab(vocab)
+        assert list(dat) == [{
+            'w': 0,
+            'ws': [2, 3],
+            'cs': [[4, 5], [5, 4]],
+            'i': 10,
+            'j': 2
+        }, {
+            'w': 1,
+            'ws': [2, 2],
+            'cs': [[5, 5], [5, 4]],
+            'i': 10,
+            'j': 2
+        }]
 
+    def test_key_error(self):
+        dat = Dataset([{'w': 'a'}])
+        vocab = {'w': {'b': 0}}
+        with pytest.raises(KeyError) as exc:
+            dat.apply_vocab(vocab)
+        assert "value 'a' not found in vocab" in str(exc.value)
 
-def test_apply_vocab_key_error():
-    dat = Dataset([{'w': 'a'}])
-    vocab = {'w': {'b': 0}}
-    with pytest.raises(KeyError) as exc:
-        dat.apply_vocab(vocab)
-    assert "value 'a' not found in vocab" in str(exc.value)
+        dat = Dataset([{'w': 10}])
+        vocab = {'w': {11: 0}}
+        with pytest.raises(KeyError) as exc:
+            dat.apply_vocab(vocab)
+        assert "value 10 not found in vocab" in str(exc.value)
 
-    dat = Dataset([{'w': 10}])
-    vocab = {'w': {11: 0}}
-    with pytest.raises(KeyError) as exc:
-        dat.apply_vocab(vocab)
-    assert "value 10 not found in vocab" in str(exc.value)
-
-
-def test_apply_vocab_with_vocab_object():
-    dat = Dataset([{
-        'ws': ['a', 'b'],
-        'cs': [['a', 'c'], ['c', 'b', 'c']]
-    }, {
-        'ws': ['b'],
-        'cs': [['b']]
-    }])
-    v = Vocab.from_samples(dat)
-    dat = dat.apply_vocab(v)
-    assert list(dat) == [{
-        'ws': [v['ws']['a'], v['ws']['b']],
-        'cs': [[v['cs']['a'], v['cs']['c']], [v['cs']['c'], v['cs']['b'], v['cs']['c']]]
-    }, {
-        'ws': [v['ws']['b']],
-        'cs': [[v['cs']['b']]]
-    }]
+    def test_with_vocab_object(self):
+        dat = Dataset([{
+            'ws': ['a', 'b'],
+            'cs': [['a', 'c'], ['c', 'b', 'c']]
+        }, {
+            'ws': ['b'],
+            'cs': [['b']]
+        }])
+        v = Vocab.from_samples(dat)
+        dat = dat.apply_vocab(v)
+        assert list(dat) == [{
+            'ws': [v['ws']['a'], v['ws']['b']],
+            'cs': [[v['cs']['a'], v['cs']['c']], [v['cs']['c'], v['cs']['b'], v['cs']['c']]]
+        }, {
+            'ws': [v['ws']['b']],
+            'cs': [[v['cs']['b']]]
+        }]
