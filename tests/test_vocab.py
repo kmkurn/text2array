33fbe3864bbc5948c47630848580601364b4aeab
Kemal Kurniawan <kemal@kkurniawan.com>
2019-01-30 22:52:46 +0700

    Add more tests and refactor a bit

--- tests/test_vocab.py
+++ tests/test_vocab.py
@@ -6,7 +6,6 @@ from text2array import Vocab
 
 
 class TestFromSamples():
-    # TODO check all possible methods of a mapping?
     def test_ok(self):
         ss = [{'w': 'c'}, {'w': 'b'}, {'w': 'a'}, {'w': 'b'}, {'w': 'c'}, {'w': 'c'}]
         vocab = Vocab.from_samples(ss)
@@ -14,12 +13,15 @@ class TestFromSamples():
         assert isinstance(vocab, Mapping)
         assert len(vocab) == 1
         assert list(vocab) == ['w']
+        with pytest.raises(KeyError):
+            vocab['ws']
 
         itos = '<pad> <unk> c b'.split()
         assert isinstance(vocab['w'], Mapping)
         assert len(vocab['w']) == len(itos)
         assert list(vocab['w']) == itos
         for i, w in enumerate(itos):
+            assert w in vocab['w']
             assert vocab['w'][w] == i
 
         assert 'foo' not in vocab['w']
@@ -30,11 +32,11 @@ class TestFromSamples():
     def test_has_vocab_for_all_str_fields(self):
         ss = [{'w': 'b', 't': 'b'}, {'w': 'b', 't': 'b'}]
         vocab = Vocab.from_samples(ss)
+        assert 'w' in vocab
         assert 't' in vocab
 
     def test_no_vocab_for_non_str(self):
         vocab = Vocab.from_samples([{'i': 10}, {'i': 20}])
-        assert 'i' not in vocab
         with pytest.raises(KeyError) as exc:
             vocab['i']
         assert "no vocabulary found for field name 'i'" in str(exc.value)
@@ -42,13 +44,7 @@ class TestFromSamples():
     def test_seq(self):
         ss = [{'ws': ['a', 'c', 'c']}, {'ws': ['b', 'c']}, {'ws': ['b']}]
         vocab = Vocab.from_samples(ss)
-
-        itos = '<pad> <unk> c b'.split()
-        assert isinstance(vocab['ws'], Mapping)
-        assert len(vocab['ws']) == len(itos)
-        assert list(vocab['ws']) == itos
-        for i, w in enumerate(itos):
-            assert vocab['ws'][w] == i
+        assert list(vocab['ws']) == '<pad> <unk> c b'.split()
 
     def test_seq_of_seq(self):
         ss = [{
@@ -59,17 +55,11 @@ class TestFromSamples():
             'cs': [['d', 'c']]
         }]
         vocab = Vocab.from_samples(ss)
-
-        itos = '<pad> <unk> d c b'.split()
-        assert isinstance(vocab['cs'], Mapping)
-        assert len(vocab['cs']) == len(itos)
-        assert list(vocab['cs']) == itos
-        for i, w in enumerate(itos):
-            assert vocab['cs'][w] == i
+        assert list(vocab['cs']) == '<pad> <unk> d c b'.split()
 
     def test_empty_samples(self):
         vocab = Vocab.from_samples([])
-        assert len(vocab) == 0
+        assert not vocab
 
     def test_empty_field_values(self):
         with pytest.raises(ValueError) as exc:
@@ -97,8 +87,8 @@ class TestFromSamples():
             't': 'c'
         }]
         vocab = Vocab.from_samples(ss, options={'w': dict(min_count=3)})
-        assert list(vocab['w']) == ['<pad>', '<unk>', 'c']
-        assert list(vocab['t']) == ['<pad>', '<unk>', 'c', 'b']
+        assert list(vocab['w']) == '<pad> <unk> c'.split()
+        assert list(vocab['t']) == '<pad> <unk> c b'.split()
 
     def test_no_unk(self):
         ss = [{
@@ -121,8 +111,8 @@ class TestFromSamples():
             't': 'c'
         }]
         vocab = Vocab.from_samples(ss, options={'w': dict(unk=None)})
-        assert list(vocab['w']) == ['<pad>', 'c', 'b']
-        assert list(vocab['t']) == ['<pad>', '<unk>', 'c', 'b']
+        assert list(vocab['w']) == '<pad> c b'.split()
+        assert list(vocab['t']) == '<pad> <unk> c b'.split()
         with pytest.raises(KeyError) as exc:
             vocab['w']['foo']
         assert "'foo' not found in vocabulary" in str(exc.value)
@@ -148,8 +138,8 @@ class TestFromSamples():
             't': 'c'
         }]
         vocab = Vocab.from_samples(ss, options={'w': dict(pad=None)})
-        assert list(vocab['w']) == ['<unk>', 'c', 'b']
-        assert list(vocab['t']) == ['<pad>', '<unk>', 'c', 'b']
+        assert list(vocab['w']) == '<unk> c b'.split()
+        assert list(vocab['t']) == '<pad> <unk> c b'.split()
 
     def test_max_size(self):
         ss = [{
@@ -172,10 +162,21 @@ class TestFromSamples():
             't': 'c'
         }]
         vocab = Vocab.from_samples(ss, options={'w': dict(max_size=1)})
-        assert list(vocab['w']) == ['<pad>', '<unk>', 'c']
-        assert list(vocab['t']) == ['<pad>', '<unk>', 'c', 'b']
+        assert list(vocab['w']) == '<pad> <unk> c'.split()
+        assert list(vocab['t']) == '<pad> <unk> c b'.split()
 
     def test_iterator_is_passed(self):
-        ss = [{'ws': ['a', 'a']}, {'ws': ['b', 'b']}]
+        ss = [{
+            'ws': ['b', 'c'],
+            'w': 'c'
+        }, {
+            'ws': ['c', 'b'],
+            'w': 'c'
+        }, {
+            'ws': ['c'],
+            'w': 'c'
+        }]
         vocab = Vocab.from_samples(iter(ss))
-        assert 'a' in vocab['ws']
+        assert 'b' in vocab['ws']
+        assert 'c' in vocab['ws']
+        assert 'c' in vocab['w']
