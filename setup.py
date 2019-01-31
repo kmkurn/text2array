b69a7ec8b68cfbf195231b946bebdbad87652051
Kemal Kurniawan <kemal@kkurniawan.com>
2019-01-31 22:06:42 +0700

    Add numpy as dependency

--- setup.py
+++ setup.py
@@ -21,5 +21,8 @@ setup(
         'Programming Language :: Python :: 3.6',
     ],
     packages=find_packages(),
+    install_requires=[
+        'numpy ~=1.16.0',
+    ],
     python_requires='>=3.6, <4',
 )
