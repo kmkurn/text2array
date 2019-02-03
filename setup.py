fee93f0dca4b94bdc3d7af0e600dede6428dc440
Kemal Kurniawan <kemal@kkurniawan.com>
2019-02-03 20:15:37 +0700

    Single-source version string

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 from pathlib import Path
 from setuptools import setup, find_packages
+import re
 
-readme = Path(__file__).resolve().parent / 'README.rst'
+here = Path(__file__).resolve().parent
+readme = (here / 'README.rst').read_text()
+version = re.search(
+    r'__version__ = (["\'])([^"\']*)\1',
+    (here / 'text2array' / '__init__.py').read_text(),
+)[2]
 
 setup(
     name='text2array',
-    version='0.0.1',
+    version=version,
     description='Convert your NLP text data to arrays!',
-    long_description=readme.read_text(),
+    long_description=readme,
     url='https://github.com/kmkurn/text2array',
     author='Kemal Kurniawan',
     author_email='kemal@kkurniawan.com',
