dd3b8eb63491cd0d6eaac54defe8309332de7ce2
Kemal Kurniawan <kemal@kkurniawan.com>
2019-03-01 08:09:43 +0700

    Remove shuffling and batching from dataset classes

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -1,8 +1,8 @@
-from typing import Iterator, Sequence
+from typing import Sequence
 
 import pytest
 
-from text2array import Batch, Dataset, Vocab
+from text2array import Dataset, Vocab
 
 
 def test_init(samples):
@@ -17,96 +17,6 @@ def test_init_samples_non_sequence():
     with pytest.raises(TypeError) as exc:
         Dataset(10)
     assert '"samples" is not a sequence' in str(exc.value)
-
-
-class TestShuffle:
-    def test_mutable_seq(self, setup_rng, dataset):
-        before = list(dataset)
-        retval = dataset.shuffle()
-        after = list(dataset)
-        assert retval is dataset
-        assert_shuffled(before, after)
-
-    def test_immutable_seq(self, setup_rng, samples):
-        dat = Dataset(tuple(samples))
-        before = list(dat)
-        retval = dat.shuffle()
-        after = list(dat)
-        assert retval is dat
-        assert_shuffled(before, after)
-
-
-class TestShuffleBy:
-    dataset = Dataset([{'i': 3}, {'i': 1}, {'i': 2}, {'i': 5}, {'i': 4}])
-
-    @staticmethod
-    def key(sample):
-        return sample['i']
-
-    def test_ok(self, setup_rng):
-        dat = self.dataset
-        before = list(dat)
-        retval = dat.shuffle_by(self.key)
-        after = list(dat)
-        assert retval is dat
-        assert_shuffled(before, after)
-
-    def test_zero_scale(self, setup_rng):
-        dat = self.dataset
-        before = list(dat)
-        dat.shuffle_by(self.key, scale=0.)
-        after = list(dat)
-        assert sorted(before, key=self.key) == after
-
-    def test_negative_scale(self, setup_rng):
-        with pytest.raises(ValueError) as exc:
-            self.dataset.shuffle_by(self.key, scale=-1)
-        assert 'scale cannot be less than 0' in str(exc.value)
-
-
-def assert_shuffled(before, after):
-    assert before != after and len(before) == len(after) and all(x in after for x in before)
-
-
-def test_batch():
-    dat = Dataset([{'i': 3}, {'i': 1}, {'i': 2}, {'i': 5}, {'i': 4}])
-    bs = dat.batch(2)
-    assert isinstance(bs, Iterator)
-    bs_lst = list(bs)
-    assert len(bs_lst) == 3
-    assert all(isinstance(b, Batch) for b in bs_lst)
-    assert list(bs_lst[0]) == [dat[0], dat[1]]
-    assert list(bs_lst[1]) == [dat[2], dat[3]]
-    assert list(bs_lst[2]) == [dat[4]]
-
-
-def test_batch_size_evenly_divides(dataset):
-    bs = dataset.batch(1)
-    bs_lst = list(bs)
-    assert len(bs_lst) == len(dataset)
-    for i in range(len(bs_lst)):
-        assert list(bs_lst[i]) == [dataset[i]]
-
-
-def test_batch_exactly():
-    dat = Dataset([{'i': 3}, {'i': 1}, {'i': 2}, {'i': 5}, {'i': 4}])
-    bs = dat.batch_exactly(2)
-    assert isinstance(bs, Iterator)
-    bs_lst = list(bs)
-    assert len(bs_lst) == 2
-    assert all(isinstance(b, Batch) for b in bs_lst)
-    assert list(bs_lst[0]) == [dat[0], dat[1]]
-    assert list(bs_lst[1]) == [dat[2], dat[3]]
-
-
-def test_batch_nonpositive_batch_size(dataset):
-    with pytest.raises(ValueError) as exc:
-        next(dataset.batch(0))
-    assert 'batch size must be greater than 0' in str(exc.value)
-
-    with pytest.raises(ValueError) as exc:
-        next(dataset.batch_exactly(0))
-    assert 'batch size must be greater than 0' in str(exc.value)
 
 
 class TestApplyVocab:
--- tests/test_stream_dataset.py
+++ tests/test_stream_dataset.py
@@ -1,8 +1,8 @@
-from typing import Iterable, Iterator
+from typing import Iterable
 
 import pytest
 
-from text2array import Batch, StreamDataset, Vocab
+from text2array import StreamDataset, Vocab
 
 
 def test_init(stream):
@@ -22,50 +22,6 @@ def test_can_be_iterated_twice(stream_dataset):
     dat_lst2 = list(stream_dataset)
     assert len(dat_lst1) == len(dat_lst2)
     assert len(dat_lst2) > 0
-
-
-def test_batch(stream_cls):
-    stream_dat = StreamDataset(stream_cls([{'i': 3}, {'i': 1}, {'i': 2}, {'i': 5}, {'i': 4}]))
-    bs = stream_dat.batch(2)
-    assert isinstance(bs, Iterator)
-    bs_lst = list(bs)
-    assert len(bs_lst) == 3
-    assert all(isinstance(b, Batch) for b in bs_lst)
-    dat = list(stream_dat)
-    assert list(bs_lst[0]) == [dat[0], dat[1]]
-    assert list(bs_lst[1]) == [dat[2], dat[3]]
-    assert list(bs_lst[2]) == [dat[4]]
-
-
-def test_batch_size_evenly_divides(stream_dataset):
-    bs = stream_dataset.batch(1)
-    dat = list(stream_dataset)
-    bs_lst = list(bs)
-    assert len(bs_lst) == len(dat)
-    for i in range(len(bs_lst)):
-        assert list(bs_lst[i]) == [dat[i]]
-
-
-def test_batch_exactly(stream_cls):
-    stream_dat = StreamDataset(stream_cls([{'i': 3}, {'i': 1}, {'i': 2}, {'i': 5}, {'i': 4}]))
-    bs = stream_dat.batch_exactly(2)
-    assert isinstance(bs, Iterator)
-    bs_lst = list(bs)
-    assert len(bs_lst) == 2
-    assert all(isinstance(b, Batch) for b in bs_lst)
-    dat = list(stream_dat)
-    assert list(bs_lst[0]) == [dat[0], dat[1]]
-    assert list(bs_lst[1]) == [dat[2], dat[3]]
-
-
-def test_batch_nonpositive_batch_size(stream_dataset):
-    with pytest.raises(ValueError) as exc:
-        next(stream_dataset.batch(0))
-    assert 'batch size must be greater than 0' in str(exc.value)
-
-    with pytest.raises(ValueError) as exc:
-        next(stream_dataset.batch_exactly(0))
-    assert 'batch size must be greater than 0' in str(exc.value)
 
 
 class TestApplyVocab:
