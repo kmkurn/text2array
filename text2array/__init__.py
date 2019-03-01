712a13db8316c90736b863efdae90c189de1810a
Kemal Kurniawan <kemal@kkurniawan.com>
2019-03-01 09:44:12 +0700

    Remove dataset classes

--- text2array/__init__.py
+++ text2array/__init__.py
@@ -1,8 +1,6 @@
 __all__ = [
     'Sample',
     'Batch',
-    'Dataset',
-    'StreamDataset',
     'Vocab',
     'BatchIterator',
     'ShuffleIterator',
@@ -10,7 +8,6 @@ __all__ = [
 __version__ = '0.0.4'
 
 from .batches import Batch
-from .datasets import Dataset, StreamDataset
 from .samples import Sample
 from .iterators import BatchIterator, ShuffleIterator
 from .vocab import Vocab
