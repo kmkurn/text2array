aa4363961cb66bee0e366ce3d836260205e5f671
Kemal Kurniawan <kemal@kkurniawan.com>
2019-03-01 07:17:37 +0700

    Let BatchIterator accept iterable of samples

--- tests/test_batch_iterator.py
+++ tests/test_batch_iterator.py
@@ -1,27 +1,35 @@
 from typing import Iterable, Sized
 
-from text2array import Batch, BatchIterator, Dataset
+import pytest
+
+from text2array import Batch, BatchIterator
 
 
-def test_init(dataset):
-    iter_ = BatchIterator(dataset)
+def test_init(samples):
+    iter_ = BatchIterator(samples)
     assert isinstance(iter_, Sized)
-    assert len(iter_) == len(dataset)
+    assert len(iter_) == len(samples)
     assert isinstance(iter_, Iterable)
     assert all(isinstance(b, Batch) for b in iter_)
     assert all(len(b) == 1 for b in iter_)
-    assert all(b[0] == s for b, s in zip(iter_, dataset))
+    assert all(b[0] == s for b, s in zip(iter_, samples))
 
 
-def test_init_kwargs(dataset):
-    dat = Dataset([{'i': i} for i in range(5)])
+def test_init_stream(stream):
+    iter_ = BatchIterator(stream)
+    with pytest.raises(TypeError):
+        len(iter_)
+
+
+def test_init_kwargs():
+    ss = [{'i': i} for i in range(5)]
     bsz = 2
-    iter_ = BatchIterator(dat, batch_size=bsz)
+    iter_ = BatchIterator(ss, batch_size=bsz)
     assert iter_.batch_size == bsz
-    assert len(iter_) == len(dataset) // bsz + (1 if len(dataset) % bsz != 0 else 0)
+    assert len(iter_) == len(ss) // bsz + (1 if len(ss) % bsz != 0 else 0)
     assert all(len(b) <= bsz for b in iter_)
 
     bs = list(iter_)
-    assert list(bs[0]) == [dat[0], dat[1]]
-    assert list(bs[1]) == [dat[2], dat[3]]
-    assert list(bs[2]) == [dat[4]]
+    assert list(bs[0]) == [ss[0], ss[1]]
+    assert list(bs[1]) == [ss[2], ss[3]]
+    assert list(bs[2]) == [ss[4]]
